4e2593a99dfc76257643ec360bd88482cd444642
Aaditya Jha <aditya.jha@amniltech.com>
2023-01-30 10:48:56 +0545

    fix: commented self-inflicted code

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const { createBot } = require('./viberSetup');
 
 var app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// commented to deploy in vercel app
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
